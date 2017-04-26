8d1f70f4086c29498d7d9ccf9f72dfec0da8ea0f
Alice-PC\Alice <drl1116@163.com>
2017-04-26 18:02:31 +0800

    更正错误代码

--- Head_First_Python3/5.函数转换为模块.py
+++ Head_First_Python3/5.函数转换为模块.py
@@ -1,9 +1,11 @@
 # -*- coding:utf-8 -*-
 # 函数转换唯模块.将之前的函数代码保存为:nester.py
 
+# 导入之前的抓换的模块
+import nester
+
 # 创建一个列表
 cast = ['Palin', 'Cleese', 'Idle', 'Jones', 'Gilliam', 'Chapman']
 
-import nester
-
-print_lol(cast)
+# 执行
+nester.print_lol(cast)
